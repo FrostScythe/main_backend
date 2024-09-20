4ea817055be3b5c6fc2d0c46f04f8a835cbc4f85
FrostScythe <maheshwariayush371@gmail.com>
2024-09-20 17:41:19 +0530

    add register controller and bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,10 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { APIError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import {uploadToCloudinary} from "../utils/cloudinary.js";
+import { uploadToCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  //upload them to cloudinary , avatar
-  //create user object - create entry in db
-  //remove password and refresh token field from response
-  //check for user creation
-  //return response
-
   const { fullname, email, username, password } = req.body;
   if ([fullname, email, username].some((field) => field?.trim() === "")) {
     throw new APIError(400, "All fields are required");
@@ -23,13 +18,40 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new APIError(409, "Username or email already exists");
   }
 
-  const avtarLocalPath = req.files?.avtar[0]?.path;
-  const coverInageLocalPath = req.files?.coverImage[0]?.path;
+  const avatarLocalPath = req.files?.avtar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  if(!avtarLocalPath){
+  if (!avatarLocalPath) {
     throw new APIError(400, "Please upload an avatar");
   }
 
+  const avatar = await uploadToCloudinary(avatarLocalPath);
+  const coverImage = await uploadToCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new APIError(400, "Please upload an avatar");
+  }
+
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    username: username.toLowerCase,
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new APIError(500, "Failed to create user");
+  }
+
+  return response
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registered successfully"));
 });
 
 export { registerUser };
